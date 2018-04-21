02a846a70b5eeb83d7844f0f1172868e8604f0ce
danfer09 <daniel.fer333@gmail.com>
2018-04-21 17:21:05 +0200

    Practica3 - 3.7 acabado

--- practica3.js
+++ practica3.js
@@ -15,6 +15,8 @@ Q.scene("level1",function(stage) {
 
 	var player = stage.insert(new Q.Mario());
 	var goomba = stage.insert(new Q.Goomba());
+	var bloopa = stage.insert(new Q.Bloopa());
+	var princess = stage.insert(new Q.Princess());
 
 	stage.add("viewport")
 		.follow(player);
@@ -22,14 +24,48 @@ Q.scene("level1",function(stage) {
 	stage.viewport.offsetX = -90;
 });
 
+Q.scene('endGame',function(stage) {
+  var box = stage.insert(new Q.UI.Container({
+    x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
+  }));
+
+  var button = box.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#CCCCCC",
+                                           label: "Play Again" }))
+  var label = box.insert(new Q.UI.Text({x:10, y: -10 - button.p.h,
+                                        label: stage.options.label }));
+  button.on("click",function() {
+    Q.clearStages();
+    Q.stageScene('level1');
+  });
+  box.fit(20);
+});
+
+Q.scene('winGame',function(stage) {
+  var box = stage.insert(new Q.UI.Container({
+    x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
+  }));
+
+  var button = box.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#CCCCCC",
+                                           label: "Play again" }))
+  var label = box.insert(new Q.UI.Text({x:10, y: -10 - button.p.h,
+                                        label: stage.options.label }));
+  button.on("click",function() {
+    Q.clearStages();
+    Q.stageScene('level1');
+  });
+  box.fit(20);
+});
+
 Q.loadTMX("level.tmx", function() {
 	Q.stageScene("level1");
 });
 
-Q.load("mario_small.png, mario_small.json, goomba.png, goomba.json", function() {
+Q.load("mario_small.png, mario_small.json, goomba.png, goomba.json, bloopa.png, bloopa.json, princess.png", function() {
 	// From a .json asset that defines sprite locations
+	Q.sheet("princess","princess.png", { tilew: 30, tileh: 48});
 	Q.compileSheets("mario_small.png","mario_small.json");
 	Q.compileSheets("goomba.png","goomba.json");
+	Q.compileSheets("bloopa.png","bloopa.json");
 	// Finally, call stageScene to run the game
 	Q.stageScene("level1");
 });
@@ -39,15 +75,16 @@ Q.Sprite.extend("Mario",{
 		this._super(p, {
 			sheet: "marioR", // Setting a sprite sheet sets sprite width and height
 			x: 150, // You can also set additional properties that can
-			y: 380 // be overridden on object creation
+			y: 450 // be overridden on object creation
 		});
 
 		this.add('2d, platformerControls');
 	},
 	step: function(dt) {
+		console.log("X: "+this.p.x+ "  Y:"+this.p.y);
 		if (this.p.y > 650) {
-			this.p.y = 380;
 			this.p.x = 150;
+			this.p.y = 380;
 		}
 	}
 });
@@ -65,10 +102,8 @@ Q.Sprite.extend("Goomba",{
 
 		this.on("bump.left,bump.right,bump.bottom",function(collision) {
 			if(collision.obj.isA("Mario")) {
-				/*Q.stageScene("endGame",1, { label: "You Died" });
-				collision.obj.destroy();*/
-				collision.obj.p.y = 380;
-				collision.obj.p.x = 150;
+				Q.stageScene("endGame",1, { label: "You Died" });
+				collision.obj.destroy();
 			}
 		});
 		// If the enemy gets hit on the top, destroy it
@@ -81,6 +116,60 @@ Q.Sprite.extend("Goomba",{
 		});
 	}
 });
+
+Q.Sprite.extend("Bloopa",{
+	init: function(p) {
+		this._super(p, {
+			sheet: "bloopa", // Setting a sprite sheet sets sprite width and height
+			x: 1478, // You can also set additional properties that can
+			y: 494, // be overridden on object creation
+			y0: 494,
+			t: 0
+		});
+
+		this.add('2d');
+
+		this.on("bump.left,bump.right,bump.bottom",function(collision) {
+			if(collision.obj.isA("Mario")) {
+				Q.stageScene("endGame",1, { label: "You Died" });
+				collision.obj.destroy();
+			}
+		});
+		// If the enemy gets hit on the top, destroy it
+		// and give the user a "hop"
+		this.on("bump.top",function(collision) {
+			if(collision.obj.isA("Mario")) {
+				this.destroy();
+				collision.obj.p.vy = -300;
+			}
+		});
+	},
+	step: function(dt) {
+		if (this.p.y == this.p.y0) {
+			this.p.vy = -280;
+		}
+	}
+});
+
+Q.Sprite.extend("Princess",{
+	init: function(p) {
+		this._super(p, {
+			sheet: "princess", // Setting a sprite sheet sets sprite width and height
+			x: 2020, // You can also set additional properties that can
+			y: 460, // be overridden on object creation
+		});
+
+		this.add('2d');
+
+		this.on("bump.left,bump.right,bump.bottom,bump.top",function(collision) {
+			if(collision.obj.isA("Mario")) {
+				Q.stageScene("winGame",1, { label: "You win!" });
+			}
+		});
+	}
+});
+
+//console.log("X: "+this.p.x+ "  Y:"+this.p.y);
 
 
 };

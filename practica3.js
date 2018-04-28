839ee7e3739509671bda3a2fcbbf4ce02e2837f8
danfer09 <daniel.fer333@gmail.com>
2018-04-28 13:43:39 +0200

    Practica3 - 3.11 acabado

--- practica3.js
+++ practica3.js
@@ -17,11 +17,18 @@ Q.scene("level1",function(stage) {
 	var goomba = stage.insert(new Q.Goomba());
 	var bloopa = stage.insert(new Q.Bloopa());
 	var princess = stage.insert(new Q.Princess());
+	var coin = stage.insert(new Q.Coin({x:200, y:475, picked: false}));
+	var coin = stage.insert(new Q.Coin({x:400, y:475, picked: false}));
+	var coin = stage.insert(new Q.Coin({x:600, y:475, picked: false}));
 
 	stage.add("viewport")
 		.follow(player);
 	stage.viewport.offsetY = 150;
 	stage.viewport.offsetX = -90;
+});
+
+Q.scene("HUD",function(stage) {
+	var score = stage.insert(new Q.Score());
 });
 
 Q.scene('mainTitle',function(stage) {
@@ -34,6 +41,7 @@ Q.scene('mainTitle',function(stage) {
   button.on("click",function() {
     Q.clearStages();
     Q.stageScene('level1');
+    Q.stageScene('HUD',1);
   });
   box.fit(20);
 });
@@ -74,13 +82,14 @@ Q.loadTMX("level.tmx", function() {
 	Q.stageScene("mainTitle");
 });
 
-Q.load("mario_small.png, mario_small.json, goomba.png, goomba.json, bloopa.png, bloopa.json, princess.png, mainTitle.png", function() {
+Q.load("mario_small.png, mario_small.json, goomba.png, goomba.json, bloopa.png, bloopa.json, princess.png, mainTitle.png, coin.png, coin.json", function() {
 	// From a .json asset that defines sprite locations
 	Q.sheet("princess","princess.png", { tilew: 30, tileh: 48});
 	Q.sheet("mainTitle","mainTitle.png");
 	Q.compileSheets("mario_small.png","mario_small.json");
 	Q.compileSheets("goomba.png","goomba.json");
 	Q.compileSheets("bloopa.png","bloopa.json");
+	Q.compileSheets("coin.png", "coin.json");
 	// Finally, call stageScene to run the game
 	Q.stageScene("mainTitle");
 });
@@ -107,6 +116,13 @@ Q.Sprite.extend("Mario",{
 										this.animate({ x: (this.p.x), vy: -300 }, 0.5, Q.Easing.Linear);
 										this.animate({ x: (this.p.x), y: (this.p.y + 60) }, 1, Q.Easing.Linear, {delay: 0.5, callback: this.reset});
 									});
+		this.on("bump.left,bump.right,bump.bottom,bump.top",function(collision) {
+			if(collision.obj.isA("Coin") && !collision.obj.picked) {
+				collision.obj.picked = true;
+				collision.obj.trigger("pickCoin");
+				Q.state.inc("score",1);
+			}
+		});
 	},
 	reset: function() {
 		this.destroy();
@@ -134,9 +150,9 @@ Q.Sprite.extend("Mario",{
 });
 
 Q.animations("mario_anim", {
-	walk_right: { frames: [0,1,2], rate: 1/15,
+	walk_right: { frames: [0,1,2], rate: 1/6,
 	flip: false, loop: true },
-	walk_left: { frames: [0,1,2], rate: 1/15,
+	walk_left: { frames: [0,1,2], rate: 1/6,
 	flip:"x", loop: true },
 	jump_right: { frames: [4], rate: 1/10, flip: false },
 	jump_left: { frames: [4], rate: 1/10, flip: "x" },
@@ -145,68 +161,83 @@ Q.animations("mario_anim", {
 	dead: { frames: [12], rate: 1, loop: false, trigger: "deadfinish"}
 });
 
+Q.Sprite.extend("Coin",{
+	init: function(p) {
+		this._super(p, {
+			sheet: "coin", // Setting a sprite sheet sets sprite width and height
+			sprite: "coin_anim"
+		});
+
+		this.add('animation, tween');
+
+		this.on("pickCoin",function(collision) {
+			this.animate({ x: (this.p.x), y: (this.p.y - 50) }, 0.5, Q.Easing.Linear, {callback: this.delete});
+		});
+	},
+
+	delete: function(){
+		this.destroy();
+	},
+
+	step:function(dt){
+		this.play("change");
+	}
+});
+
+Q.animations("coin_anim", {
+	change: { frames: [0,1,2], rate: 1/4, flip: false, loop: true }
+});
+
 Q.Sprite.extend("Goomba",{
 	init: function(p) {
 		this._super(p, {
 			sheet: "goomba", // Setting a sprite sheet sets sprite width and height
+			sprite: "goomba_anim",
 			x: 1600, // You can also set additional properties that can
 			y: 380, // be overridden on object creation
 			vx: 100
 		});
 
-		this.add('2d, aiBounce');
+		this.add('2d, aiBounce, animation,defaultEnemy');
+	},
 
-		this.on("bump.left,bump.right,bump.bottom",function(collision) {
-			if(collision.obj.isA("Mario")) {
-				//collision.obj.p.vy = -300;
-				//collision.obj.p.vx = 0;
-				collision.obj.trigger("destroyMario");
-			}
-		});
-		// If the enemy gets hit on the top, destroy it
-		// and give the user a "hop"
-		this.on("bump.top",function(collision) {
-		if(collision.obj.isA("Mario")) {
-			this.destroy();
-			collision.obj.p.vy = -300;
+	step:function(dt){
+		if(this.p.vx != 0){
+			this.play("walk");
 		}
-		});
 	}
+});
+
+Q.animations("goomba_anim", {
+	walk: { frames: [0,1], rate: 1/6, flip: false, loop: true },
+	dead: { frames: [2], rate: 1, loop: false, trigger: "destroy"}
 });
 
 Q.Sprite.extend("Bloopa",{
 	init: function(p) {
 		this._super(p, {
 			sheet: "bloopa", // Setting a sprite sheet sets sprite width and height
+			sprite: "bloopa_anim",
 			x: 1478, // You can also set additional properties that can
 			y: 494, // be overridden on object creation
 			y0: 494,
 			t: 0
 		});
 
-		this.add('2d');
-
-		this.on("bump.left,bump.right,bump.bottom",function(collision) {
-			if(collision.obj.isA("Mario")) {
-				//collision.obj.p.vy = -300;
-				//collision.obj.p.vx = 0;
-				collision.obj.trigger("destroyMario");
-			}
-		});
-		// If the enemy gets hit on the top, destroy it
-		// and give the user a "hop"
-		this.on("bump.top",function(collision) {
-			if(collision.obj.isA("Mario")) {
-				this.destroy();
-				collision.obj.p.vy = -300;
-			}
-		});
+		this.add('2d, animation, defaultEnemy');
 	},
 	step: function(dt) {
+		this.play("walk");
 		if (this.p.y == this.p.y0) {
 			this.p.vy = -280;
 		}
+
 	}
+});
+
+Q.animations("bloopa_anim", {
+	walk: { frames: [0,1], rate: 1/4, flip: false, loop: true },
+	dead: { frames: [2], rate: 1, loop: false, trigger: "destroy"}
 });
 
 Q.Sprite.extend("Princess",{
@@ -227,7 +258,59 @@ Q.Sprite.extend("Princess",{
 	}
 });
 
+Q.UI.Text.extend("Score",{
+	init: function(p) {
+		this._super({
+			label: "Score: 0",
+			x:60,
+			y: 0,
+			score: 0
+		});
+		Q.state.set("score",0);
 
+		Q.state.on("change.score",this,"score");
+	},
+
+	score: function(score) {
+		this.p.score = score;
+		this.p.label = "Score: " + this.p.score;
+	}
+});
+
+Q.component("defaultEnemy",{
+	added: function() {
+		this.entity.on("bump.left,bump.right,bump.bottom","collisionMario");
+		// If the enemy gets hit on the top, destroy it
+		// and give the user a "hop"
+		this.entity.on("bump.top","deadEnemy");
+
+		this.entity.on("dead","animationDeadEnemy" );
+
+		this.entity.on("destroy", "destroyEnemy");
+	},
+
+	extend:{
+		collisionMario: function(collision) {
+							if(collision.obj.isA("Mario")) {
+								collision.obj.trigger("destroyMario");
+							}	
+		},
+		deadEnemy: function(collision) {
+			if(collision.obj.isA("Mario")) {
+				this.trigger("dead");
+				collision.obj.p.vy = -300;
+			}
+		},
+		animationDeadEnemy: function() {
+			this.p.vx = 0;
+			this.play("dead", 1);
+		},
+		destroyEnemy: function() {
+			this.destroy();
+		}
+	}
+
+});
 
 /*Q.animations("player_anim", {
 	walk_right: { frames: [0,1,2,3,4,5,6,7,8,9,10], rate: 1/15,
